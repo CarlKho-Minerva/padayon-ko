68aee283705c2b8590c6861aa39014a617a01d54
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-08-05 01:34:14 +0800

    .

--- backend/facebook_graph_api/test_FB_Post.py
+++ backend/facebook_graph_api/test_FB_Post.py
@@ -9,8 +9,8 @@ def create_facebook_post(message, access_token, page_id):
 
 
 if __name__ == "__main__":
-    FACEBOOK_ACCESS_TOKEN = "EAALn51qddPIBO1b3w0XH4Y0MfQ2eRkAc1lxadC1xuZCoVk4KguZCG8csusAxjsRJyEqeHzOBo5VnvZAyMzLfqQhDQ4wnjJDR0beNVQkK0l5Jglkg8yl1lr5tkSz62rcVQVHZAvn32UdEToxq1aWsbVfZCl8SWv7ZA8I53FRbMndyCHMseyvcF0rL1WUpeBeAVAGGjdDNlPSag7aXff9KPF6yCBDJkSz5ocZBD4HjTXk"
-    FACEBOOK_PAGE_ID = "394193433777965"
+    FACEBOOK_ACCESS_TOKEN = ""
+    FACEBOOK_PAGE_ID = ""
     TEST_MESSAGE = (
         "Hello World - Testing Facebook Graph API! From VS Code with Python this time"
     )

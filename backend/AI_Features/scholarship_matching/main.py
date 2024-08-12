c505b9c346ee971f7dea5247a3df0f94d34eafcd
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-08-12 23:29:17 +0800

    profile_to_match: debug prints for demo

--- backend/AI_Features/scholarship_matching/main.py
+++ backend/AI_Features/scholarship_matching/main.py
@@ -9,26 +9,34 @@ from notion_handle_status import update_scholarship_status
 
 
 def main():
-    print("Starting main process")
+    print("\n### Starting main process ###\n")
+
+    print("\n### Obtaining database IDs ###\n")
     user_db_id = get_database_id("Chilipepper DB")
     scholarship_db_id = get_database_id("Scholarship List for Padayun Ko")
 
+    print("\n### Extracting user profile ###\n")
     user_profile = extract_user_profile(user_db_id)
     if not user_profile:
+        print("\n### No user profile found. Exiting process. ###\n")
         return
 
+    print("\n### Optimizing search based on user profile ###\n")
     optimized_query = optimize_query(user_profile)
 
+    print("\n### Retrieving scholarship summaries ###\n")
     scholarship_summaries = get_scholarship_summaries(scholarship_db_id)
     if not scholarship_summaries:
+        print("\n### No scholarships found. Exiting process. ###\n")
         return
 
+    print("\n### Finding best matches for the user ###\n")
     best_match_indices = find_best_matches(optimized_query, scholarship_summaries)
 
-    # Update the status of the top 3 matched scholarships
+    print("\n### Updating the status of the top 3 matched scholarships ###\n")
     for index in best_match_indices:
         scholarship = scholarship_summaries[index]
-        print(f"Updating status for scholarship: {scholarship['title']}")
+        print(f"\n### Updating status for scholarship: {scholarship['title']} ###\n")
         update_scholarship_status(scholarship_db_id, scholarship["page_id"])
 
 
